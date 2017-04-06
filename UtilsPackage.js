188ac3c51fea11ed85ea322d6de56639449a44ea
Alex19950209 <1060856020@qq.com>
2017-04-06 21:17:38 +0800

    utils

--- UtilsPackage.js
+++ UtilsPackage.js
@@ -1,6 +1,6 @@
 
 var Utils = {
-    quickSort:function(arr){//快速排序法
+    quickSort: function(arr){//快速排序法
         if(arr.length < 1) {return arr;}
         var pivotIndex = Math.floor(arr.length / 2),
             pivot = arr.splice(pivotIndex,1)[0],
@@ -15,7 +15,7 @@ var Utils = {
         }
         return this.quickSort(left).concat([pivot],this.quickSort(right));
     },
-    unique:function(arr){//数组去重
+    unique: function(arr){//数组去重
         if(arr.length < 1) {return arr;}
         var hash = {}, arr1 = [];
         for(var i = 0;i < arr.length;i++){
@@ -26,7 +26,7 @@ var Utils = {
         }
         return arr1;
     },
-    setCookie:function(name,value,path,exp,type){//存储cookie
+    setCookie: function(name,value,path,exp,type){//存储cookie
         if(type){
             localStorage.setItem(name,value);
         }else{
@@ -36,7 +36,7 @@ var Utils = {
         }
 
     },
-    getCookie:function(name,type){//获取cookie
+    getCookie: function(name,type){//获取cookie
         if (type) {
             return localStorage.getItem(name);
         } else {
@@ -44,8 +44,55 @@ var Utils = {
             if (arr != null) return unescape(arr[2]);
             return null;
         } 
+    },
+    timeStamp: function(stamp){
+        if(!stamp) return;
+            stamp = typeof stamp == 'string'? stamp : stamp.toString();
+            if(stamp.length !== 10 && stamp.length !== 13) return 'error';
+            stamp = stamp.length == 10 ? parseInt(stamp) * 1000 : parseInt(stamp);
+            
+            var now = new Date(),
+                month = now.getMonth() +　1,
+                days = now.getDate(),
+                hours = now.getHours(),
+                minutes = now.getMinutes(),
+                stime = new Date(stamp),
+                syear = stime.getFullYear(),
+                smonth = stime.getMonth() + 1,
+                sdays = stime.getDate(),
+                sday = stime.getDay(),
+                shours = stime.getHours(),
+                sminutes = stime.getMinutes(),
+            change = function(obj){
+                var j,
+                    n = [0,1,2,3,4,5,6],
+                    a = ['日','一','二','三','四','五','六'];
+                for(var i = 0; i < n.length;i++){
+                    if(obj == n[i]){
+                        j = i;
+                        break
+                    }
+                }
+                return a[i];
+            },
+            getDate = function(){
+                if(smonth !== month) return syear + '年' + smonth + '月' + sdays + '日';
+                if(days - sdays == 1) return '昨天';
+                if( 1 < days - sdays && days - sdays <= 7) return '星期' + change(sday);
+            },
+            getquantum = function(){
+                if( 0 <= shours && shours < 6) return '凌晨';
+                if( 6 <= shours && shours< 12) return '上午';
+                if( 12 <= shours && shours< 24) return '下午';
+            },
+            getHours = function(){
+                return shours = shours > 12 ? shours - 12 : shours;
+            },
+            double = function(o){
+                return o = o < 10 ? '0' + o : o;
+            }
+            return getDate() + ' ' + getquantum() + ' ' + getHours() + ':'+ double(minutes);       
     }
-    
     
     
     
